87a60e88d800550b7a1551f50a8aa0fdacf2c454
Евгений Боганов <evgenijboganov@MacBook-Pro-Evgenij.local>
2022-02-16 09:01:01 +0300

    Add props and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import img from './courses-5.jpeg'
+import img from './courses-5.jpeg';
 class ContentFirst extends React.Component{
   render(){
     console.log(this.props);
@@ -13,7 +13,10 @@ class ContentFirst extends React.Component{
   >
     Learn React
   </a>;
-    return(<div>{link}
+    return(<div> <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>{link}
     qewrqwerqerqerq</div>)
   }
 }
@@ -26,9 +29,13 @@ function Img(props){
   )
 }
 function Text(props){
+
   return(
     <div>Hallo
-      <div> My name's Jenya</div>
+      <p> My name's {props.name}</p>
+      <p>I am {props.age} years old</p>
+      <p>I study {props.obj.one} and {props.obj.two}</p>
+      <button  >{props.function()} Click </button>
     </div>
   )
 }
@@ -45,7 +52,68 @@ class NewText extends React.Component{
 class Newlink extends React.Component{
   render(){
     return (
-      <a class="App-link" target="_blank" href="https://github.com/Doommez/ReactFirst" rel='GITHUB'>GITHUB</a>
+      <a className="App-link" target="_blank" href="https://github.com/Doommez/ReactFirst" rel='GITHUB'>GITHUB</a>
+    )
+  }
+}
+function hi(params) {
+  console.log("hi")
+}
+class User extends React.Component{
+  state={
+    count:0
+  }
+  handclick(){
+    this.setState(state=>({count: ++state.count}))
+  }
+  render(){
+    const {name,age}=this.props;
+    const {count}=this.state;
+    return(
+      <div>
+<p>{name} "13412341234"</p>
+<p>{age}qewrqwerqwer</p>
+<p>{count}</p>
+<button onClick={()=>this.handclick()}>Click Me</button>
+</div>
+    )
+  }
+}
+class Clock extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+    date:new Date()
+  }
+  }
+  tick(){
+    
+    this.setState({
+      date: new Date()
+    });
+    
+   
+   
+  }
+  componentDidMount() {
+    this.timerId=setInterval(this.tick,1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
+  render(){
+    let {date}=this.state
+    return(
+      <div>
+        <p>
+          Time Now:
+        </p>
+        <p>
+          {date.toLocaleTimeString()}
+        </p>
+      </div>
     )
   }
 }
@@ -54,16 +122,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       <Text/>
-       <ContentFirst name="qqweewweqerq" age={32}/>
-       <ContentFirst />
-       <NewText someText="qwerqwerqwerqwerqwerqw"/>
-       <Newlink/>
-       <Img visible={false}/>
+        <Clock/>
+        <User/>
+       <ContentFirst/>
+       <Text name="jenya" age={24} obj={{one:"English",two:"React"}} function={function hallo(){console.log(2+2,"Hello")}}/>
       </header>
     </div>
   );
